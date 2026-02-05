25cfa8769ec9880425e20ce5caf311881cc994b3
Sadnesm <Blep@mail>
2026-02-06 01:53:13 +1000

    Base Funcionality added

---
+++ Battleships/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SeaBattle
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pbxSelf = new PictureBox();
+            pbxEnemy = new PictureBox();
+            txtIP = new TextBox();
+            txtPort = new TextBox();
+            btnHost = new Button();
+            btnConnect = new Button();
+            lblStatus = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbxSelf).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbxEnemy).BeginInit();
+            SuspendLayout();
+            // 
+            // pbxSelf
+            // 
+            pbxSelf.BackColor = Color.White;
+            pbxSelf.Location = new Point(49, 128);
+            pbxSelf.Name = "pbxSelf";
+            pbxSelf.Size = new Size(301, 301);
+            pbxSelf.TabIndex = 0;
+            pbxSelf.TabStop = false;
+            // 
+            // pbxEnemy
+            // 
+            pbxEnemy.BackColor = Color.White;
+            pbxEnemy.Location = new Point(390, 128);
+            pbxEnemy.Name = "pbxEnemy";
+            pbxEnemy.Size = new Size(301, 301);
+            pbxEnemy.TabIndex = 1;
+            pbxEnemy.TabStop = false;
+            // 
+            // txtIP
+            // 
+            txtIP.Location = new Point(49, 12);
+            txtIP.Name = "txtIP";
+            txtIP.Size = new Size(125, 27);
+            txtIP.TabIndex = 2;
+            txtIP.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            txtPort.Location = new Point(49, 45);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(125, 27);
+            txtPort.TabIndex = 3;
+            txtPort.Text = "8888";
+            // 
+            // btnHost
+            // 
+            btnHost.Location = new Point(390, 12);
+            btnHost.Name = "btnHost";
+            btnHost.Size = new Size(118, 29);
+            btnHost.TabIndex = 4;
+            btnHost.Text = "Создать игру";
+            btnHost.UseVisualStyleBackColor = true;
+            btnHost.Click += btnHost_Click;
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(390, 47);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(118, 29);
+            btnConnect.TabIndex = 5;
+            btnConnect.Text = "Подключиться";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(568, 15);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(146, 20);
+            lblStatus.TabIndex = 6;
+            lblStatus.Text = "Расставьте корабли";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblStatus);
+            Controls.Add(btnConnect);
+            Controls.Add(btnHost);
+            Controls.Add(txtPort);
+            Controls.Add(txtIP);
+            Controls.Add(pbxEnemy);
+            Controls.Add(pbxSelf);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)pbxSelf).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbxEnemy).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pbxSelf;
+        private PictureBox pbxEnemy;
+        private TextBox txtIP;
+        private TextBox txtPort;
+        private Button btnHost;
+        private Button btnConnect;
+        private Label lblStatus;
+    }
+}
---
+++ Battleships/MainForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    public partial class MainForm : Form
+    {
+        private GameLogic game = new GameLogic();
+        private GameStatus status = GameStatus.Placement;
+        private TcpClient client;
+        private NetworkStream stream;
+        private const int CellSize = 30;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private async void btnHost_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(txtPort.Text));
+                listener.Start();
+                lblStatus.Text = "Ожидание игрока...";
+                client = await listener.AcceptTcpClientAsync();
+                stream = client.GetStream();
+                status = GameStatus.MyTurn;
+                lblStatus.Text = "Ваш ход";
+                StartReceiving();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+                await client.ConnectAsync(txtIP.Text, int.Parse(txtPort.Text));
+                stream = client.GetStream();
+                status = GameStatus.EnemyTurn;
+                lblStatus.Text = "Ожидание соперника...";
+                StartReceiving();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private async void StartReceiving()
+        {
+            byte[] buffer = new byte[1024];
+            while (client != null && client.Connected)
+            {
+                try
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    string json = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    var msg = JsonSerializer.Deserialize<GameMessage>(json);
+
+                    this.Invoke((MethodInvoker)(() => HandleIncomingMessage(msg)));
+                }
+                catch { break; }
+            }
+        }
+
+        private void HandleIncomingMessage(GameMessage msg)
+        {
+            if (msg.Type == "Shot")
+            {
+                string result = game.ProcessEnemyShot(msg.X, msg.Y);
+                Send(new GameMessage { Type = "Result", X = msg.X, Y = msg.Y, Status = result });
+                if (result == "Miss") status = GameStatus.MyTurn;
+            }
+            else if (msg.Type == "Result")
+            {
+                game.EnemyField[msg.X, msg.Y] = (msg.Status == "Miss") ? CellState.Miss : CellState.Hit;
+                if (msg.Status == "Miss") status = GameStatus.EnemyTurn;
+            }
+            RefreshUI();
+        }
+
+        private void Send(GameMessage msg)
+        {
+            string json = JsonSerializer.Serialize(msg);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void pbxEnemy_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (status != GameStatus.MyTurn) return;
+
+            int x = e.X / CellSize;
+            int y = e.Y / CellSize;
+
+            if (x >= 0 && x < 10 && y >= 0 && y < 10 && game.EnemyField[x, y] == CellState.Empty)
+            {
+                Send(new GameMessage { Type = "Shot", X = x, Y = y });
+            }
+        }
+
+        private void RefreshUI()
+        {
+            pbxSelf.Invalidate();
+            pbxEnemy.Invalidate();
+            lblStatus.Text = (status == GameStatus.MyTurn) ? "Ваш ход" : "Ход противника";
+        }
+    }
+}
